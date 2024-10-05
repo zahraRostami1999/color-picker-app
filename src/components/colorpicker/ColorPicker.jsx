bed977925cf7be9ab72895342337fbd82a323754
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-05 13:29:41 +0330

    edit ColorPicker component: change style class name to title box and pickBox

--- src/components/colorpicker/ColorPicker.jsx
+++ src/components/colorpicker/ColorPicker.jsx
@@ -10,10 +10,10 @@ function ColorPicker() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.title}>
+        <div className={styles.titleBox}>
           <h2>Pick Your background color:</h2>
         </div>
-        <div>
+        <div className={styles.pickBox}>
           <input
             type="color"
             vlaue={color}
