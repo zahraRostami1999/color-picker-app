1e261867aaabce811fe117be0452478daeab7dc9
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-05 13:27:55 +0330

    edit ColorPicker componnet: add input and p tag for select and show color

--- src/components/colorpicker/ColorPicker.jsx
+++ src/components/colorpicker/ColorPicker.jsx
@@ -1,10 +1,30 @@
-import styles from './ColorPicker.module.css';
+import { useState } from "react";
+import styles from "./ColorPicker.module.css";
 
-function ColorPicker(){
-    return (
-        <>
-        </>
-    );
+function ColorPicker() {
+  const [color, setColor] = useState("#fff");
+  const handleChangeColor = (e) => {
+    setColor(e.target.value);
+    document.body.style.backgroundColor = e.target.value;
+  };
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h2>Pick Your background color:</h2>
+        </div>
+        <div>
+          <input
+            type="color"
+            vlaue={color}
+            onChange={handleChangeColor}
+            name="color"
+          />
+          <p>hex: {color}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ColorPicker;
